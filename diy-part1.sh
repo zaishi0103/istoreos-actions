721f8cbabc7da4add55aab08f5b5339ee02400e7
zaishi0103 <34189712+zaishi0103@users.noreply.github.com>
2024-03-05 15:08:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+ echo 'src-git ipsec-vpnd https://github.com/Ivaneus/luci-app-ipsec-vpnd' >>feeds.conf.default
